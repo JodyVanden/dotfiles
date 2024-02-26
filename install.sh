3fdc742b26c013a10d926194457f62c96924473e
Jody V <jvandenschrick@squareup.com>
2024-02-26 21:05:10 +1100

    Update install.sh
    using bash instead of ruby

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ backup() {
 # Check for Homebrew and install if we don't have it
 echo "Check for Homebrew and install if we don't have it"
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update Homebrew recipes
